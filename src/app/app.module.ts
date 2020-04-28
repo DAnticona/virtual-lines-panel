af6769065d468043833605a5190bf7b85cac7ef8
David Anticona <david.anticona@webmas.pe>
2020-04-27 22:33:17 -0500

    Input, output y viewchild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,12 @@ import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    APP_ROUTES,
-    PagesModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, LoginComponent, RegisterComponent],
+	imports: [BrowserModule, APP_ROUTES, PagesModule, FormsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,28 +2,33 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { PAGES_ROUTES } from './pages.routes';
 
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+// n2-charts
+import { ChartsModule } from 'ng2-charts';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
 @NgModule({
-    declarations: [
-        DashboardComponent,
-        ProgressComponent,
-        Graficas1Component,
-        PagesComponent,
-    ],
-    exports: [
-        DashboardComponent,
-        ProgressComponent,
-        Graficas1Component,
-        PagesComponent,
-    ],
-    imports: [
-        SharedModule,
-        PAGES_ROUTES
-    ]
+	declarations: [
+		DashboardComponent,
+		ProgressComponent,
+		Graficas1Component,
+		PagesComponent,
+		IncrementadorComponent,
+		GraficoDonaComponent,
+	],
+	exports: [DashboardComponent, ProgressComponent, Graficas1Component, PagesComponent],
+	imports: [SharedModule, PAGES_ROUTES, FormsModule, ChartsModule, BrowserModule, CommonModule],
 })
-export class PagesModule { }
+export class PagesModule {}
